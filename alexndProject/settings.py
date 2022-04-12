0ddb0be1e86d33f76312170504ee5c779fc14d8b
Alexandru Oprea <alexx.oprea@yahoo.com>
2022-04-12 19:20:04 +0300

    next version + requirements.txt

--- alexndProject/settings.py
+++ alexndProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-bjrv5s3gts_6_jo@ry)_gm1-x=xcih(w15qn05_!dp!9-=k7l5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eshopproject22.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
